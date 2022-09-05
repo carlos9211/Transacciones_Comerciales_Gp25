9e1043a27c8821d24337c69e9d10e7caaaeaee10
carlos9211 <82066869+carlos9211@users.noreply.github.com>
2022-09-04 21:32:31 -0500

    interfaz de django
    User grupo25
    password: Grupo25*

--- Proyecto_Transacciones/appreto1/models.py
+++ Proyecto_Transacciones/appreto1/models.py
@@ -15,16 +15,22 @@ class Empleado(models.Model):
     Nombre=models.CharField(max_length=30)
     Apellido=models.CharField(max_length=30)
     Documento=models.IntegerField()
-    rol=models.IntegerField()
+    Id_rol=models.IntegerField()
     correo=models.CharField(max_length=50)
     celular=models.CharField(max_length=10)
     NombreEmpresa=models.ForeignKey(Empresa,on_delete=models.CASCADE)
 
 #TABLA DE USUARIOS
+class Usuarios(models.Model):
+    Nombre=models.CharField(max_length=20)
+    Apellido=models.CharField(max_length=30)
+    Documento=models.IntegerField()
 
 
 #TABLA DE ROLES
-
+class Roles(models.Model):
+    Id_Rol=models.IntegerField(primary_key=True)
+    rol=models.CharField(max_length=30)
 
 #TABLA DE MOVIMIENTOS
 
