d737f18ff81e6ef9155e0e2684b7bd42fcb29b5b
cpuello <cheche0685@gmail.com>
2022-09-07 20:19:45 -0500

    Actualizando Bases de Datos

--- Proyecto_Transacciones/appreto1/models.py
+++ Proyecto_Transacciones/appreto1/models.py
@@ -1,23 +1,32 @@
+from datetime import datetime
 from django.db import models
 
 
 #TABLA DE EMPRESAS
 class Empresa(models.Model):
-    Id_empresa=models.IntegerField()
-    Nit=models.IntegerField()
-    NombreEmpresa=models.CharField(max_length=30)
-    Direccion=models.CharField(max_length=50)
-    Telefono=models.IntegerField()
-
+    IdEmpresa=models.IntegerField(max_length=20)
+    Nit=models.CharField(max_length=10)
+    DigitoVerificacion=models.CharField(max_length=1)
+    Nombre=models.CharField(max_length=200)
+    Telefono=models.CharField(max_length=20)
+    Direccion=models.CharField(max_length=100)
+    Correo=models.CharField(max_length=100)
+    FechaCreacion=models.DateTimeField(default=datetime.now)
+    FechaModificacion=models.DateTimeField(default=datetime.now)
+    
 
 #TABLA DE EMPLEADOS
 class Empleado(models.Model):
-    Nombre=models.CharField(max_length=30)
-    Apellido=models.CharField(max_length=30)
-    Documento=models.IntegerField()
-    Id_rol=models.IntegerField()
-    correo=models.CharField(max_length=50)
-    celular=models.CharField(max_length=10)
+    IdEmpleado=models.IntegerField(max_length=20)
+    Nombre=models.CharField(max_length=100)
+    Apellido=models.CharField(max_length=100)
+    Codigo=models.CharField(max_length=200)
+    Telefono=models.CharField(max_length=20)
+    Direccion=models.CharField(max_length=100)
+    Correo=models.CharField(max_length=100)
+    Cargo=models.CharField(max_length=100)
+    FechaCreacion=models.DateTimeField(default=datetime.now)
+    FechaModificacion=models.DateTimeField(default=datetime.now)
     NombreEmpresa=models.ForeignKey(Empresa,on_delete=models.CASCADE)
 
 #TABLA DE USUARIOS
