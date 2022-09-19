4039f6d18a29fac061d043d0039c8b10bc8a312a
carlos9211 <82066869+carlos9211@users.noreply.github.com>
2022-09-18 21:25:39 -0500

    modificar datos
    esta bien el codigo pero aun no modifica datos, toca seguir revisando clase del 6 de sept

--- ProyectoCiclo3/pciclo3/appciclo/urls.py
+++ ProyectoCiclo3/pciclo3/appciclo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import EmpresaView, EmpresaView
 
 urlpatterns=[
-    path('empresa/',EmpresaView.as_view(),name='Listar')
+    path('empresa/',EmpresaView.as_view(),name='Listar'),
+    path('empresa/<int:doc>',EmpresaView.as_view(),name='actualizar')
 
-]
+] 
--- ProyectoCiclo3/pciclo3/appciclo/views.py
+++ ProyectoCiclo3/pciclo3/appciclo/views.py
@@ -5,15 +5,38 @@ from django.views import View
 from .models import Empresa
 from django.http.response import JsonResponse
 
+
+#creamos una clase con la tabla de empresas que permite ingresar datos
+#get se envian datos por medio de la url
 class EmpresaView(View):
     def get(self,request):
         cli=list(Empresa.objects.values())
         datos={'listadoempresa':cli}
         return JsonResponse (datos)
 
+#post enviamos los datos a la tabla por medio del body
 
     def post(self,request):
         datos=json.loads(request.body)
         Empresa.objects.create(IdEmpresa=datos["IdEmpresa"],Nombre=datos["Nombre"],Nit=datos["Nit"],Ciudad=datos["Ciudad"],Direccion=datos["Direccion"],Telefono=datos["Telefono"],SectorProductivo=datos["SectorProductivo"])
         return JsonResponse(datos)
+
+#put se usa para modificar los datos
+    def put(self,request,doc):
+        datos=json.loads(request.body)
+        cli=list(Empresa.objects.filter(IdEmpresa=doc).values())
+        if len(cli)>0:
+            Empresas=Empresa.objects.get(IdEmpresa=doc)
+            Empresas.Nombre=datos["Nombre"]
+            Empresas.Nit=datos["Nit"]
+            Empresas.Ciudad=datos["Ciudad"]
+            Empresas.Direccion=datos["Direccion"]
+            Empresas.Telefono=datos["Telefono"]
+            Empresas.SectorProductivo=datos["SectorProductivo"]
+            Empresas.save()
+            mensaje={"Respuesta":"Datos Actualizado"}
+        else:
+            mensaje={"Respuesta":"Datos No Encontrados"}
+        return JsonResponse(mensaje)
+        
 # Create your views here.
